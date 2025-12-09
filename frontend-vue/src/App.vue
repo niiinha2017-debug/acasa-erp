2f91b254c4a7fee15ca4619d76b10e041d4c8aac
niiinha2017-debug <niiinha.2017@gmail.com>
2025-12-09 17:42:24 -0300

    atualizar

--- frontend-vue/src/App.vue
+++ frontend-vue/src/App.vue
@@ -1,20 +1,7 @@
 <script setup>
-import { ref, computed } from 'vue';
 import Login from './views/Login.vue';
-import Dashboard from './views/Dashboard.vue';
-import { AppConfig } from './config';
-
-// Verifica se tem token salvo
-const token = localStorage.getItem(AppConfig.STORAGE_KEYS.TOKEN);
-const usuarioLogado = ref(!!token); // true se tiver token, false se não
-
-// Função simples para atualizar a tela quando logar
-const aoLogar = () => {
-  usuarioLogado.value = true;
-};
 </script>
 
 <template>
-  <Dashboard v-if="usuarioLogado" />
-  <Login v-else @login-sucesso="aoLogar" />
+  <Login />
 </template>
