69007a8f582be2a3fba7e47bcc1f176a440f526d
niiinha2017-debug <niiinha.2017@gmail.com>
2025-12-10 10:48:44 -0300

    Corrigindo estrutura e removendo arquivos antigos

--- frontend-vue/src/main.js
+++ frontend-vue/src/main.js
@@ -2,6 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 // AGORA O CAMINHO ESTÁ CERTO:
-import './assets/css/Main.css' 
+//import './assets/css/Main.css' 
 
 createApp(App).mount('#app')
