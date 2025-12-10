7a4f5be89fddfa88e143ab7748e051706a834812
niiinha2017-debug <niiinha.2017@gmail.com>
2025-12-10 16:51:12 -0300

    atualizacao 2025-12-10 16:51:12

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,12 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { Controller } from '@nestjs/common';
 
-@Controller('auth')
-export class AuthController {
-  constructor(private readonly authService: AuthService) {}
-
-  @Post('login')
-  async login(@Body() body: { email: string; password: string }) {
-    return this.authService.login(body.email, body.password);
-  }
-}
+@Controller('users')
+export class UsersController {}
--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from '../user.entity'; // Importando a tabela que mapeamos
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])], // <--- A Mágica acontece aqui
-  controllers: [UsersController],
   providers: [UsersService],
+  controllers: [UsersController]
 })
 export class UsersModule {}
