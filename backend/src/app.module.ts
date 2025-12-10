f95f27f27b25ec146a158823b0ec856b37dfe2e2
niiinha2017-debug <niiinha.2017@gmail.com>
2025-12-10 12:22:16 -0300

    Configurando conexao MySQL no Backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    // 1. Carrega as variáveis do arquivo .env (onde estará a senha)
+    ConfigModule.forRoot({ isGlobal: true }),
+
+    // 2. Configura a conexão com o MySQL
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST || 'localhost',
+      port: 3306,
+      username: process.env.DB_USER || 'root',
+      password: process.env.DB_PASSWORD, // A senha virá do arquivo .env
+      database: process.env.DB_NAME || 'acasa_erp',
+      entities: [], 
+      synchronize: true, // Cria as tabelas sozinho (bom para dev)
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
