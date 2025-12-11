eec8cbf551405535587a7385d2288eb78aa669b6
niiinha2017-debug <niiinha.2017@gmail.com>
2025-12-11 17:10:03 -0300

    fix: change bcrypt to bcryptjs

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { JwtService } from '@nestjs/jwt';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
+
 
 @Injectable()
 export class AuthService {
