404cc8aeffc84b3b16e42c5d9867449caaa80fd1
niiinha2017-debug <niiinha.2017@gmail.com>
2025-12-18 21:24:16 -0300

    mensagem

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,6 +3,9 @@ import { storage } from '@/utils/storage'
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 })
 
 api.interceptors.request.use((config) => {
