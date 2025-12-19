fc8b55d698b5583a6a6db249fd63df7aa5b1d636
niiinha2017-debug <niiinha.2017@gmail.com>
2025-12-19 09:46:29 -0300

    chore: funcionou. eu sobrevivi. seguimos.

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,8 +1,8 @@
-// src/users/users.service.ts
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
+import * as bcrypt from 'bcryptjs'; // Importe o bcrypt
 
 @Injectable()
 export class UsersService {
@@ -11,9 +11,22 @@ export class UsersService {
     private usersRepository: Repository<User>,
   ) {}
 
+  // Função que você já tem
   async findByEmail(email: string): Promise<User | null> {
     return this.usersRepository.findOne({
       where: { email },
     });
+  }
+
+  // NOVA FUNÇÃO: Cria usuário com senha protegida
+  async create(userData: Partial<User>): Promise<User> {
+    if (userData.password) {
+      // Gera o "sal" e cria o hash da senha
+      const salt = await bcrypt.genSalt(10);
+      userData.password = await bcrypt.hash(userData.password, salt);
+    }
+
+    const newUser = this.usersRepository.create(userData);
+    return this.usersRepository.save(newUser);
   }
 }
