b5a90a85ecc2255f7ca08541472f332f600a1d97
niiinha2017-debug <niiinha.2017@gmail.com>
2025-12-22 09:56:22 -0300

    fix(auth): importar UsersModule no AuthModule

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,13 +1,17 @@
-import { Global, Module } from '@nestjs/common';
-import { PassportModule } from '@nestjs/passport';
-import { JwtModule } from '@nestjs/jwt';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
-import { JwtStrategy } from './jwt.strategy';
+import { Module, Global } from '@nestjs/common'
+import { PassportModule } from '@nestjs/passport'
+import { JwtModule } from '@nestjs/jwt'
+
+import { AuthService } from './auth.service'
+import { AuthController } from './auth.controller'
+import { JwtStrategy } from './jwt.strategy'
+
+import { UsersModule } from '../users/users.module'
 
 @Global()
 @Module({
   imports: [
+    UsersModule, // 🔥 ESSENCIAL
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
       secret: process.env.JWT_SECRET,
@@ -16,10 +20,10 @@ import { JwtStrategy } from './jwt.strategy';
   ],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy],
-  exports: [PassportModule, JwtStrategy],
+  exports: [AuthService, PassportModule],
 })
 export class AuthModule {
   constructor() {
-    console.log('🧩 AuthModule carregado');
+    console.log('🧩 AuthModule carregado')
   }
 }
