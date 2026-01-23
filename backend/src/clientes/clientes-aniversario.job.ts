a6dad52bac08ab2e143d942e8192917f9d3c2487
niiinha2017-debug <niiinha.2017@gmail.com>
2026-01-23 09:30:25 -0300

    mensagem clara

--- backend/src/clientes/clientes-aniversario.job.ts
+++ backend/src/clientes/clientes-aniversario.job.ts
@@ -13,7 +13,7 @@ export class ClientesAniversarioJob {
   ) {}
 
   // todo dia às 08:00 (São Paulo)
-  @Cron('/10 * * *', { timeZone: 'America/Sao_Paulo' })
+  @Cron('0 0 10 * * *', { timeZone: 'America/Sao_Paulo' })
   async executar() {
     const hoje = new Date()
     const dia = hoje.getDate()
