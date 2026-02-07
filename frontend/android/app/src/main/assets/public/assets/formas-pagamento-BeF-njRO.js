256cbe25ce1e8c4b4c598cc82fbe8f28a6a7f04d
niiinha2017-debug <niiinha.2017@gmail.com>
2026-02-06 22:30:25 -0300

    chore: remove android generated assets from repo

--- frontend/android/app/src/main/assets/public/assets/formas-pagamento-BeF-njRO.js
+++
@@ -1,2 +0,0 @@
-const e=[{key:"DINHEIRO",label:"Dinheiro"},{key:"PIX",label:"PIX"},{key:"DEBITO",label:"Cartão de Débito"},{key:"CREDITO",label:"Cartão de Crédito"},{key:"BOLETO",label:"Boleto"},{key:"CHEQUE",label:"Cheque"},{key:"TRANSFERENCIA",label:"Transferência"}];export{e as F};
-//# sourceMappingURL=formas-pagamento-BeF-njRO.js.map
